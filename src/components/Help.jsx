f90e97d92afaaaa1265ab81f7a37fe5a7de70500
hassanalisheikh098 <143602821+hassanalisheikh098@users.noreply.github.com>
2025-02-18 21:55:32 +0500

    Update Help.jsx

--- src/components/Help.jsx
+++ src/components/Help.jsx
@@ -2,6 +2,7 @@
 import React from "react";
 import { ContainerScroll } from "../pages/contribute";
 import { Instagram, Linkedin, Mail } from "lucide-react"
+import pic from '../assets/pic.png'; // Adjust the path as necessary
 
 const SocialIcons = () => {
   return (
@@ -43,7 +44,7 @@ export function HeroScrollDemo() {
         }
       >
         <img
-          src="https://scontent.flhe5-1.fna.fbcdn.net/v/t39.30808-6/477324413_1739136723298798_5827494122248110193_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeFz2HqO7lgNEEx38oGYQ4N3Lqn5OwQNS4Yuqfk7BA1LhhIrgKmQfbibf1PEVbWqGoqDWdTas7pcrt8U-6YR9AN2&_nc_ohc=ZgDecVlpdDoQ7kNvgGzKHoY&_nc_oc=Adjv_hCncHWTRTAxQh5D4zXpMts0Sqq5xzbqJn_jwEAggFNWcL2VNsDrpb_qizzyG6TYX_ZWWn2tvqwjWgValaIQ&_nc_zt=23&_nc_ht=scontent.flhe5-1.fna&_nc_gid=At36JAhRpmRB-zi4V58G5oX&oh=00_AYBdvjenXRc-Fwpm6jzEoPYn0cXaWdsRlt3vM-Z63_dDfw&oe=67B2BDA3"
+          src= {pic}
           alt="hero"
           height={1700}
           width={800}
